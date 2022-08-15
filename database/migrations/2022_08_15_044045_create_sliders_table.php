6b191782051bf6959694d203aadf3c3727009d12
Ison <ison.dbordeos@gmail.com>
2022-08-15 17:04:22 +0800

    update dependencies

--- database/migrations/2022_08_15_044045_create_sliders_table.php
+++ database/migrations/2022_08_15_044045_create_sliders_table.php
@@ -13,11 +13,11 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('home_slides', function (Blueprint $table) {
+        Schema::create('sliders', function (Blueprint $table) {
             $table->id();
             $table->string('title')->nullable();
             $table->string('short_title')->nullable();
-            $table->string('home_slide')->nullable();
+            $table->string('slider_image')->nullable();
             $table->string('video_url')->nullable();
             $table->timestamps();
         });
@@ -30,6 +30,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('home_slides');
+        Schema::dropIfExists('sliders');
     }
 };
